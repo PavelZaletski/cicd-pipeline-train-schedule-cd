d95e20fe0669465c4d7e182498f67a14813336ae
PavelZaletski <pavel.zaletski@gmail.com>
2021-10-10 13:46:18 +0300

    Update trains.js

--- routes/trains.js
+++ routes/trains.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var low = require('lowdb')
 var FileSync = require('lowdb/adapters/FileSync')
 
-var adapter = new FileSync('data/trains.json')
+var adapter = new FileSync('./data/trains.json')
 var db = low(adapter)
 
 /* GET trains listing. */
